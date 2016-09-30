b18b1a30c37bdec3680d9dc496821f50c9d23c24
Daniel <danielallendeutsch@gmail.com>
2016-09-30 11:44:34 -0500

    adds user login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,28 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(email: user_params[:email])
+    if @user.nil?
+      flash[:error] = "There is no account associated with that email address"
+      redirect_to new_session_path
+    elsif @user.authenticate(user_params[:password])
+      flash[:success] = "Login Successful"
+      redirect_to new_session_path
+    else
+      flash[:alert] = "Incorrect Password"
+      redirect_to new_session_path
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
