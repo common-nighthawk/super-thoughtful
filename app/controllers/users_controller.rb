25c105500c9d9d0e137e46de416cc40380e33476
Daniel <danielallendeutsch@gmail.com>
2016-10-02 11:43:30 -0500

    adds user sign-up.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Account Created"
+      redirect_to new_session_path
+    else
+      flash[:error] = @user.errors.full_messages.to_sentence
+      redirect_to new_session_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :password, length: { minimum: 8 }, allow_nil: true
 end
