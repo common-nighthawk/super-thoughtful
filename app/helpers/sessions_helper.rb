6a185f5c9cffaf09a57633a8ce1785a0243696ff
Daniel <danielallendeutsch@gmail.com>
2016-10-02 12:23:02 -0500

    adds birthdate to users.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+  MIN_AGE = 18
+  MAX_AGE = 100
+
+  def start_year
+    (Date.today - MIN_AGE.years).year
+  end
+
+  def end_year
+    (Date.today - MAX_AGE.years).year
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :birthdate, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }, allow_nil: true
 end
